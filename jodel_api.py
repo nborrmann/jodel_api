4e7c94ae4b9b5f5129fda060d04dbb231eb46367
nborrmann <n.borrmann@googlemail.com>
2016-10-26 18:10:59 +0200

    updated api secret

--- jodel_api.py
+++ jodel_api.py
@@ -73,11 +73,11 @@ class JodelAccount:
                "%".join(sorted(urlparse(url).query.replace("=", "%").split("&"))),
                json.dumps(payload, separators=(',',':'))]
 
-        secret = bytearray([ord(c) for c in "MowxMGuZnoXYgVoAlqmcgUPxdbszEBpBXpjpccbg"])
+        secret = bytearray([ord(c) for c in "zQTjsuEvclFMwHxzCEPCtMzClaayFhcgmHtuMwTG"])
         signature = hmac.new(secret, "%".join(req).encode("utf-8"), sha1).hexdigest().upper()
 
         headers['X-Authorization'] = 'HMAC ' + signature
-        headers['X-Client-Type'] = 'android_4.21.1'
+        headers['X-Client-Type'] = 'android_4.23.1'
         headers['X-Timestamp'] = timestamp
         headers['X-Api-Version'] = '0.2'
 
