ac7eeb27504cdbcf4b29e50fc7c56ea7d230d69e
nbo <nborrmann@users.noreply.github.com>
2016-11-22 13:02:59 +0100

    Key for 4.27.1

--- jodel_api.py
+++ jodel_api.py
@@ -73,11 +73,11 @@ class JodelAccount:
                "%".join(sorted(urlparse(url).query.replace("=", "%").split("&"))),
                json.dumps(payload, separators=(',',':'))]
 
-        secret = bytearray([ord(c) for c in "UaFmcOKyiqHtqOalardBxjapyUJeFkvsgsAhplzt"])
+        secret = bytearray([ord(c) for c in "VwJHzYUbPjGiXWauoVNaHoCWsaacTmnkGwNtHhjy"])
         signature = hmac.new(secret, "%".join(req).encode("utf-8"), sha1).hexdigest().upper()
 
         headers['X-Authorization'] = 'HMAC ' + signature
-        headers['X-Client-Type'] = 'android_4.25.3'
+        headers['X-Client-Type'] = 'android_4.27.1'
         headers['X-Timestamp'] = timestamp
         headers['X-Api-Version'] = '0.2'
 
