9bd1439ee0c42efa22e9abc561b3b37a8b3247aa
nbo <nborrmann@users.noreply.github.com>
2017-05-09 16:29:22 +0200

    refactor/cleanup and some new api methods (#21)
    * refactor/cleanup
    
    * add methods for interacting with sticky posts
    
    * add method to set user profile
    
    * update readme

--- src/jodel_api.py
+++ src/jodel_api.py
@@ -7,12 +7,12 @@ install_aliases()
 
 import base64
 import datetime
+from hashlib import sha1
 import hmac
 import json
 import random
-from hashlib import sha1
-from urllib.parse import urlparse
 import requests
+from urllib.parse import urlparse
 
 s = requests.Session()
 
@@ -20,7 +20,7 @@ s = requests.Session()
 class JodelAccount:
     post_colors = ['9EC41C', 'FF9908', 'DD5F5F', '8ABDB0', '06A3CB', 'FFBA00']
 
-    api_url = "https://api.go-tellm.com/api%s"
+    api_url = "https://api.go-tellm.com/api{}"
     client_id = '81e8a76e-1e02-4d17-9ba0-8a7020261b26'
     secret = 'XJwOzRfxCXsVLRkutOHyiexqDnDutUCnTrECnvpw'.encode('ascii')
     version = '4.43.1'
@@ -51,8 +51,8 @@ class JodelAccount:
                 raise Exception("Error creating new account: " + str(r))
 
     def _send_request(self, method, endpoint, params=None, payload=None, **kwargs):
-        url = self.api_url % endpoint
-        headers = {'User-Agent': 'Jodel/%s Dalvik/2.1.0 (Linux; U; Android 5.1.1; )' % self.version,
+        url = self.api_url.format(endpoint)
+        headers = {'User-Agent': 'Jodel/{} Dalvik/2.1.0 (Linux; U; Android 5.1.1; )'.format(self.version),
                    'Accept-Encoding': 'gzip',
                    'Content-Type': 'application/json; charset=UTF-8',
                    'Authorization': 'Bearer ' + self.access_token if self.access_token else None}
@@ -60,7 +60,7 @@ class JodelAccount:
         for _ in range(3):
             self._sign_request(method, url, headers, params, payload)
             resp = s.request(method=method, url=url, params=params, json=payload, headers=headers, **kwargs)
-            if resp.status_code != 502: # Retry on error 502 "Bad Gateway"
+            if resp.status_code != 502:  # Retry on error 502 "Bad Gateway"
                 break
 
         try:
@@ -85,7 +85,7 @@ class JodelAccount:
         signature = hmac.new(self.secret, "%".join(req).encode("utf-8"), sha1).hexdigest().upper()
 
         headers['X-Authorization'] = 'HMAC ' + signature
-        headers['X-Client-Type'] = 'android_%s' % self.version
+        headers['X-Client-Type'] = 'android_{}'.format(self.version)
         headers['X-Timestamp'] = timestamp
         headers['X-Api-Version'] = '0.2'
 
@@ -96,6 +96,10 @@ class JodelAccount:
                 "loc_coordinates": {"lat": lat, "lng": lng}, 
                 "country": country if country else "DE", 
                 "name": name if name else city}
+
+    def get_account_data(self):
+        return {'expiration_date': self.expiration_date, 'distinct_id': self.distinct_id,
+                'refresh_token': self.refresh_token, 'device_uid': self.device_uid, 'access_token': self.access_token}
 
     def refresh_all_tokens(self, **kwargs):
         """ Creates a new account with random ID if self.device_uid is not set. Otherwise renews all tokens of the
@@ -130,7 +134,7 @@ class JodelAccount:
 
     def verify_account(self):
         r = self.get_user_config()
-        if r[0] == 200 and r[1]['verified'] == True:
+        if r[0] == 200 and r[1]['verified']:
             print("Account is already verified.")
             return
 
@@ -149,24 +153,70 @@ class JodelAccount:
                 continue
 
             r = self.submit_captcha(r[1]['key'], answer)
-            if r[0] == 200 and r[1]['verified'] == True:
+            if r[0] == 200 and r[1]['verified']:
                 print("Account successfully verified.")
                 return
             else:
                 print("Verification failed. Retrying ...")
 
-            
-    def get_account_data(self):
-        return {'expiration_date': self.expiration_date, 'distinct_id': self.distinct_id,
-                'refresh_token': self.refresh_token, 'device_uid': self.device_uid, 'access_token': self.access_token}
+    # ################# #
+    # GET POSTS METHODS #
+    # ################# #
 
-    def set_location(self, lat, lng, city, country=None, name=None, **kwargs):
-        self.lat, self.lng, self.location_dict = lat, lng, self._get_location_dict(lat, lng, city, country, name)
-        return self._send_request("PUT", "/v2/users/location", payload={"location": self.location_dict}, **kwargs)
+    def _get_posts(self, post_types="", skip=0, limit=60, after=None, mine=False, hashtag=None, channel=None, pictures=False, **kwargs):
+        category = "mine" if mine else "hashtag" if hashtag else "channel" if channel else "location"
+        url_params = {"api_version": "v2" if not (hashtag or channel or pictures) else "v3",
+                      "pictures_posts": "pictures" if pictures else "posts",
+                      "category": category,
+                      "post_types": post_types}
+        params = {"lat": self.lat,
+                  "lng": self.lng,
+                  "skip": skip,
+                  "limit": limit,
+                  "hashtag": hashtag,
+                  "channel": channel,
+                  "after": after}
+
+        url = "/{api_version}/{pictures_posts}/{category}/{post_types}".format(**url_params)
+        return self._send_request("GET", url, params=params, **kwargs)
+
+    def get_posts_recent(self, skip=0, limit=60, after=None, mine=False, hashtag=None, channel=None, **kwargs):
+        return self._get_posts('', skip, limit, after, mine, hashtag, channel, **kwargs)
+
+    def get_posts_popular(self, skip=0, limit=60, after=None, mine=False, hashtag=None, channel=None, **kwargs):
+        return self._get_posts('popular', skip, limit, after, mine, hashtag, channel, **kwargs)
+
+    def get_posts_discussed(self, skip=0, limit=60, after=None, mine=False, hashtag=None, channel=None, **kwargs):
+        return self._get_posts('discussed', skip, limit, after, mine, hashtag, channel, **kwargs)
+
+    def get_pictures_recent(self, skip=0, limit=60, after=None, **kwargs):
+        return self._get_posts('', skip, limit, after, pictures=True, **kwargs)
+
+    def get_pictures_popular(self, skip=0, limit=60, after=None, **kwargs):
+        return self._get_posts('popular', skip, limit, after, pictures=True, **kwargs)
+
+    def get_pictures_discussed(self, skip=0, limit=60, after=None, **kwargs):
+        return self._get_posts('discussed', skip, limit, after, pictures=True, **kwargs)
+
+    def get_my_pinned_posts(self, skip=0, limit=60, after=None, **kwargs):
+        return self._get_posts('pinned', skip, limit, after, True, **kwargs)
+
+    def get_my_replied_posts(self, skip=0, limit=60, after=None, **kwargs):
+        return self._get_posts('replies', skip, limit, after, True, **kwargs)
+
+    def get_my_voted_posts(self, skip=0, limit=60, after=None, **kwargs):
+        return self._get_posts('votes', skip, limit, after, True, **kwargs)
+
+    def get_newsfeed(self, after=None, **kwargs):
+        return self._send_request('GET', '/v3/posts/newsfeed', params={'after': after}, **kwargs)
+
+    # ################### #
+    # SINGLE POST METHODS #
+    # ################### #
 
     def create_post(self, message=None, imgpath=None, b64img=None, color=None, ancestor=None, channel="", **kwargs):
         if not imgpath and not message and not b64img:
-            raise Exception("One of message or imgpath must not be null.")
+            raise ValueError("One of message or imgpath must not be null.")
 
         payload = {"color": color if color else random.choice(self.post_colors),
                    "location": self.location_dict,
@@ -182,57 +232,56 @@ class JodelAccount:
 
         return self._send_request("POST", '/v3/posts/', payload=payload, **kwargs)
 
+    def get_post_details(self, post_id, **kwargs):
+        return self._send_request("GET", '/v2/posts/{}/'.format(post_id), **kwargs)
+
+    def get_post_details_v3(self, post_id, skip=0, **kwargs):
+        return self._send_request("GET", '/v3/posts/{}/details'.format(post_id),
+                                  params={'details': 'true', 'reply': skip}, **kwargs)
+
     def upvote(self, post_id, **kwargs):
-        return self._send_request("PUT", '/v2/posts/%s/upvote/' % post_id, **kwargs)
+        return self._send_request("PUT", '/v2/posts/{}/upvote/'.format(post_id), **kwargs)
 
     def downvote(self, post_id, **kwargs):
-        return self._send_request("PUT", '/v2/posts/%s/downvote/' % post_id, **kwargs)
+        return self._send_request("PUT", '/v2/posts/{}/downvote/'.format(post_id), **kwargs)
 
     def give_thanks(self, post_id, **kwargs):
-        return self._send_request("POST", '/v3/posts/%s/giveThanks' % post_id, **kwargs)
-
-    def get_post_details(self, message_id, **kwargs):
-        return self._send_request("GET", '/v2/posts/%s/' % message_id, **kwargs)
-
-    def get_post_details_v3(self, message_id, skip=0, **kwargs):
-        return self._send_request("GET", '/v3/posts/%s/details' % message_id, params={'details': 'true', 'reply': skip}, **kwargs)
-
-    def _get_posts(self, post_types="", skip=0, limit=60, after="", mine=False, hashtag="", channel="", **kwargs):
-        category = "mine" if mine else "hashtag" if hashtag else "channel" if channel else "location"
-        params = {"lat": self.lat,
-                  "lng": self.lng,
-                  "skip": skip,
-                  "limit": limit,
-                  "hashtag": hashtag,
-                  "channel": channel,
-                  "after": after}
-        url = "/v%s/posts/%s/%s" % ("2" if not (hashtag or channel) else "3", category, post_types)
-
-        return self._send_request("GET", url, params=params, **kwargs)
-
-    def get_posts_recent(self, skip=0, limit=60, after="", mine=False, hashtag="", channel="", **kwargs):
-        return self._get_posts('', skip, limit, after, mine, hashtag, channel, **kwargs)
-
-    def get_posts_popular(self, skip=0, limit=60, after="", mine=False, hashtag="", channel="", **kwargs):
-        return self._get_posts('popular', skip, limit, after, mine, hashtag, channel, **kwargs)
-
-    def get_posts_discussed(self, skip=0, limit=60, after="", mine=False, hashtag="", channel="", **kwargs):
-        return self._get_posts('discussed', skip, limit, after, mine, hashtag, channel, **kwargs)
-
-    def get_my_pinned_posts(self, skip=0, limit=60, after="", **kwargs):
-        return self._get_posts('pinned', skip, limit, after, True, **kwargs)
-
-    def get_my_replied_posts(self, skip=0, limit=60, after="", **kwargs):
-        return self._get_posts('replies', skip, limit, after, True, **kwargs)
-
-    def get_my_voted_posts(self, skip=0, limit=60, after="", **kwargs):
-        return self._get_posts('votes', skip, limit, after, True, **kwargs)
-
-    def get_newsfeed(self, after=""):
-        return self._send_request('GET', '/v3/posts/newsfeed', params={'after': after})
+        return self._send_request("POST", '/v3/posts/{}/giveThanks'.format(post_id), **kwargs)
 
     def get_share_url(self, post_id, **kwargs):
-        return self._send_request("POST", "/v3/posts/%s/share" % post_id, **kwargs)
+        return self._send_request("POST", "/v3/posts/{}/share".format(post_id), **kwargs)
+
+    def pin(self, post_id, **kwargs):
+        return self._send_request("PUT", "/v2/posts/{}/pin".format(post_id), **kwargs)
+
+    def unpin(self, post_id, **kwargs):
+        return self._send_request("PUT", "/v2/posts/{}/unpin".format(post_id), **kwargs)
+
+    def enable_notifications(self, post_id, **kwargs):
+        return self._send_request("PUT", "/v2/posts/{}/notifications/enable".format(post_id), **kwargs)
+
+    def disable_notifications(self, post_id, **kwargs):
+        return self._send_request("PUT", "/v2/posts/{}/notifications/disable".format(post_id), **kwargs)
+
+    def delete_post(self, post_id, **kwargs):
+        return self._send_request("DELETE", "/v2/posts/{}".format(post_id), **kwargs)
+
+    # ################### #
+    # STICKY POST METHODS #
+    # ################### #
+
+    def upvote_sticky_post(self, post_id, **kwargs):
+        return self._send_request("PUT", "/v3/stickyposts/{}/up".format(post_id), **kwargs)
+
+    def downvote_sticky_post(self, post_id, **kwargs):
+        return self._send_request("PUT", "/v3/stickyposts/{}/down".format(post_id), **kwargs)
+
+    def dismiss_sticky_post(self, post_id, **kwargs):
+        return self._send_request("PUT", "/v3/stickyposts/{}/dismiss".format(post_id), **kwargs)
+
+    # #################### #
+    # NOTIFICATION METHODS #
+    # #################### #
 
     def get_notifications(self, **kwargs):
         return self._send_request("PUT", "/v3/user/notifications", **kwargs)
@@ -242,23 +291,15 @@ class JodelAccount:
 
     def notification_read(self, post_id=None, notification_id=None, **kwargs):
         if post_id:
-            return self._send_request("PUT", "/v3/user/notifications/post/%s/read" % post_id, **kwargs)
+            return self._send_request("PUT", "/v3/user/notifications/post/{}/read".format(post_id), **kwargs)
         elif notification_id:
-            return self._send_request("PUT", "/v3/user/notifications/%s/read" % notification_id, **kwargs)
+            return self._send_request("PUT", "/v3/user/notifications/{}/read".format(notification_id), **kwargs)
         else:
-            raise Exception("One of post_id or notification_id must not be null.") 
+            raise ValueError("One of post_id or notification_id must not be null.") 
 
-    def pin(self, post_id, **kwargs):
-        return self._send_request("PUT", "/v2/posts/%s/pin" % post_id, **kwargs)
-
-    def unpin(self, post_id, **kwargs):
-        return self._send_request("PUT", "/v2/posts/%s/unpin" % post_id, **kwargs)
-
-    def enable_notifications(self, post_id, **kwargs):
-        return self._send_request("PUT", "/v2/posts/%s/notifications/enable" % post_id, **kwargs)
-
-    def disable_notifications(self, post_id, **kwargs):
-        return self._send_request("PUT", "/v2/posts/%s/notifications/disable" % post_id, **kwargs)
+    # ############### #
+    # CHANNEL METHODS #
+    # ############### #
 
     def get_recommended_channels(self, **kwargs):
         return self._send_request("GET", "/v3/user/recommendedChannels", **kwargs)
@@ -266,27 +307,43 @@ class JodelAccount:
     def get_channel_meta(self, channel, **kwargs):
         return self._send_request("GET", "/v3/user/channelMeta", params={"channel": channel}, **kwargs)
 
-    def follow_channel(self, channel):
-        return self._send_request("PUT", "/v3/user/followChannel", params={"channel": channel})
+    def follow_channel(self, channel, **kwargs):
+        return self._send_request("PUT", "/v3/user/followChannel", params={"channel": channel}, **kwargs)
 
-    def unfollow_channel(self, channel):
-        return self._send_request("PUT", "/v3/user/unfollowChannel", params={"channel": channel})
+    def unfollow_channel(self, channel, **kwargs):
+        return self._send_request("PUT", "/v3/user/unfollowChannel", params={"channel": channel}, **kwargs)
 
-    def get_user_config(self, **kwargs):
-        return self._send_request("GET", "/v3/user/config", **kwargs)
+    # ############ #
+    # USER METHODS #
+    # ############ #
+
+    def set_location(self, lat, lng, city, country=None, name=None, **kwargs):
+        self.lat, self.lng, self.location_dict = lat, lng, self._get_location_dict(lat, lng, city, country, name)
+        return self._send_request("PUT", "/v2/users/location", payload={"location": self.location_dict}, **kwargs)
+
+    def set_user_profile(self, user_type=None, gender=None, age=None, **kwargs):
+        allowed_user_types = ["high_school", "high_school_graduate", "student", "apprentice", "employee", "other", None]
+        if user_type and user_type not in allowed_user_types:
+            raise ValueError("user_type must be one of {}.".format(allowed_user_types))
+
+        if gender not in ["m", "f", None]:
+            raise ValueError("gender must be either m or f.")
+
+        return self._send_request("PUT", "/v3/user/profile", payload={"user_type": user_type, "gender": gender, "age": age}, **kwargs)
 
     def get_karma(self, **kwargs):
         return self._send_request("GET", "/v2/users/karma", **kwargs)
 
-    def delete_post(self, post_id, **kwargs):
-        return self._send_request("DELETE", "/v2/posts/%s" % post_id, **kwargs)
+    def get_user_config(self, **kwargs):
+        return self._send_request("GET", "/v3/user/config", **kwargs)
 
     def get_captcha(self, **kwargs):
         return self._send_request("GET", "/v3/user/verification/imageCaptcha", **kwargs)
 
     def submit_captcha(self, key, answer, **kwargs):
-        payload = {'key':key, 'answer':answer}
+        payload = {'key': key, 'answer': answer}
         return self._send_request("POST", "/v3/user/verification/imageCaptcha", payload=payload, **kwargs)
+
 
 # helper function to mock input
 def obtain_input(text):
